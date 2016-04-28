1d14e32f6f07a6eb282299aaf3f2451fa69f353f
Mike Hingley <Computa_Mike@hotmail.com>
2016-04-28 22:06:15 +0100

    Initial Commit

---
+++ UnitTestLibrary/Javascript.test.js
@@ -0,0 +1,54 @@
+﻿module("Javascript Tests",
+    {
+        setup: function () {
+ 
+        },
+        teardown: function(){
+ 
+
+
+        }
+    }
+    );
+
+
+// Logging call backs
+QUnit.moduleDone(function (details) {
+
+    console.log("Module Complete : " + details.name);
+    console.log("Passed : " + details.passed);
+    console.log("Failed : " + details.failed);
+    console.log("Total : " + details.total);
+    console.log("Runtime : " + details.runtime);
+});
+
+//Register a callback to fire whenever the test suite begins.
+QUnit.begin(function( details ) {
+   console.log( "Test amount:", details.totalTests );
+});
+
+// This function is a call back whenever an assertion completes.
+QUnit.log(function (details) {
+ //   console.log("Log: ", details.result, details.message);
+});
+ 
+
+// TESTS ------
+
+QUnit.test("hello test", function (assert) {
+    
+    assert.ok(1 == "1", "Passed!");
+});
+
+QUnit.test("Get All Products", function (assert) {
+    function square(x) {
+        return x * x;
+    }
+
+    var result = square(2);
+    assert.equal(result, 4, "square(2) equals 4");
+
+});
+ 
+
+ 
